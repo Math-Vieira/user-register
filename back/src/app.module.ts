4f8c16f5a4cf5d1177be09870636ddd2f58261b0
math-vieira <matheusvieira9153@gmail.com>
2024-03-02 11:36:10 -0300

    chore (back): remove unnecessary files

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MONGODB_URI } from './shared/utils/env-constants';
 
 @Module({
   imports: [ConfigModule.forRoot(), MongooseModule.forRoot(MONGODB_URI)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
