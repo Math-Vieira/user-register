ecc44088251717165219dbcfc52bf12460fd0547
math-vieira <matheusvieira9153@gmail.com>
2024-03-02 12:38:08 -0300

    chore (back): add class validator config

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './shared/utils/env-constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.CORS_ORIGIN || '*',
+    },
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
   await app.listen(PORT);
 }
 bootstrap();
