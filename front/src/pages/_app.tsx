86bcebfc10967d56b5b13d63218ca3e45c4658f7
math-vieira <matheusvieira9153@gmail.com>
2024-03-04 13:36:43 -0300

    chore (front): add basic styles and create login page file

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-const a = 2;
+
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
