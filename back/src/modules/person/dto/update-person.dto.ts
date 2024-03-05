b0dee8ef7de59e379071745494ac7f63f6e63e93
math-vieira <matheusvieira9153@gmail.com>
2024-03-05 11:05:03 -0300

    refactor (back): remove unnecessary prop from updatePersonDto

--- back/src/modules/person/dto/update-person.dto.ts
+++ back/src/modules/person/dto/update-person.dto.ts
@@ -5,8 +5,4 @@ export class UpdatePersonDto extends CreatePersonDto {
   @IsString({ message: 'a propriedade "_id" deve ser uma string' })
   @IsNotEmpty({ message: 'a propriedade "_id" não pode ser vazia' })
   _id: string;
-
-  @IsString({ message: 'a propriedade "user_id" deve ser uma string' })
-  @IsNotEmpty({ message: 'a propriedade "user_id" não pode ser vazia' })
-  user_id: string;
 }
