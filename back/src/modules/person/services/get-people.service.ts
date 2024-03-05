42cd705e79360769ae776f4110a61e7e94e7a711
math-vieira <matheusvieira9153@gmail.com>
2024-03-05 01:08:20 -0300

    fix (back): fix pagination logic

--- back/src/modules/person/services/get-people.service.ts
+++ back/src/modules/person/services/get-people.service.ts
@@ -7,8 +7,8 @@ export class GetPeopleService {
   constructor(private readonly personRepository: PersonRepository) {}
   async exec(page: number, token: string) {
     const user_id = obtainUserIdFromToken(token);
-    const skip = (page - 1) * 10;
-    const limit = 10; //Number of people per page
+    const limit = 7; //Number of people per page
+    const skip = (page - 1) * limit;
 
     const totalPeople =
       await this.personRepository.getTotalPeopleByUserId(user_id);
